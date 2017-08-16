4df7fd4de8254e2bb8f69a8bf7c6c0e54d55e8ec
oscar <oscarcased2017@gmail.com>
2017-08-16 10:17:25 -0500

    conectando local con remoto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  nombre ="Fernando";
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a=0.234;
+  salario=1234.5;
+
+  objeto:Object  = {
+      foo: "bar",
+      baz: "qux",
+      edad: 500,
+      nested: {
+        xyz: "3",
+        numbers: "[1, 2, 3, 4, 5]"
+      }
+  };
+
 }
